eac1eec1cb4d54d819050bb1fa3fcddbcf3dab46
Atharv Mohite <notam4040@gmail.com>
2026-08-01 17:07:25 +0530

    Some changes

--- doot-backend/doot-backend/src/main/java/com/doot/backend/Repository/TransactionRepository.java
+++ doot-backend/doot-backend/src/main/java/com/doot/backend/Repository/TransactionRepository.java
@@ -4,6 +4,10 @@ import com.doot.backend.Entity.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions,Long> {
+    List<Transactions> findTop20ByOrderByTransactionIdDesc();
+    boolean existsByPacketHash(String packetHash);
 }
