6389c300bfb1a09d4ff913094489d981fbb5f6bd
Atharv Mohite <notam4040@gmail.com>
2026-08-01 17:36:12 +0530

    Configuration file is created

---
+++ doot-backend/doot-backend/src/main/java/com/doot/backend/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.doot.backend.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class AppConfig {
+}
